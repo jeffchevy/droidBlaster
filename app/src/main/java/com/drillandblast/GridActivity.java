f0658414c74509553c0af17f3eb2846a56ed0e88
Zach <zbug_zlp@msn.com>
2016-04-12 12:41:24 -0600

    Grid Activity changes, and Brent's screw up

--- app/src/main/java/com/drillandblast/GridActivity.java
+++ app/src/main/java/com/drillandblast/GridActivity.java
@@ -3,6 +3,7 @@ package com.drillandblast;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -17,25 +18,20 @@ public class GridActivity extends AppCompatActivity {
         TableLayout tbl = (TableLayout) findViewById(R.id.drill_log_table);
         //TableLayout.LayoutParams tblparams = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT);
 
-        int rowCount = 50;
-        int colCount = 50;
+        int rowCount = 200;
+        int colCount = 400;
 
         for(int i=0; i<rowCount;i++) {
             TableRow row = new TableRow(this);
             row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
 
             for(int j=0; j<colCount; j++) {
-                TextView textView = new TextView(this);
-                textView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-                if(j%2 == 0) {
-                    textView.setText("Even");
-                    textView.setBackgroundColor(Color.RED);
-                }
-                else {
-                    textView.setText("Odd");
-                    textView.setBackgroundColor(Color.BLUE);
-                }
-                row.addView(textView);
+                Button holeButton = new Button(this);
+                holeButton.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                //TextView textView = new TextView(this);
+                //textView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                holeButton.setText("Drill");
+                row.addView(holeButton);
             }
             tbl.addView(row, new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
         }
